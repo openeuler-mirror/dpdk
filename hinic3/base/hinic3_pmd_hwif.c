e524b5ec23cace5f50dc6ead756bd08f26e8078a
chenyi221 <chenyi221@huawei.com>
2026-04-21 19:47:07 +0800

    [feat] sp230新增 vf设备编号

--- hinic3/base/hinic3_pmd_hwif.c
+++ hinic3/base/hinic3_pmd_hwif.c
@@ -153,7 +153,8 @@
 #define HINIC3_IS_VF_DEV(pdev)		((pdev)->id.device_id == HINIC3_DEV_ID_VF_SP620  || \
 					(pdev)->id.device_id == HINIC3_DEV_ID_VF_BP2_620 || \
 					(pdev)->id.device_id == HINIC3_DEV_ID_VF_BP3_620 || \
-					(pdev)->id.device_id == HINIC3_DEV_ID_VF_SP230)
+					(pdev)->id.device_id == HINIC3_DEV_ID_VF_SP230 || \
+					(pdev)->id.device_id == HINIC3_DEV_ID_VF_SP230_OLD)
 
 u32 hinic3_hwif_read_reg(struct hinic3_hwif *hwif, u32 reg)
 {
