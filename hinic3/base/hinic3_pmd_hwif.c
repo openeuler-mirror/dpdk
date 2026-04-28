ef116aad145dfe56b72afd9f1bc48338effa9443
wangxin <wangxin679@h-partners.com>
2026-04-28 10:42:49 +0800

    [fix] SP560 VID DID 适配
    [描述信息] SP560 vendor_id device_id 适配
    [修改说明] nic_dev->id_table 中添加对应的 pci_device

--- hinic3/base/hinic3_pmd_hwif.c
+++ hinic3/base/hinic3_pmd_hwif.c
@@ -152,7 +152,9 @@
 #define HINIC3_IS_VF_DEV(pdev) ( \
 	((pdev)->id.device_id == HINIC3_DEV_ID_VF_SP620) || \
 	((pdev)->id.device_id == HINIC3_DEV_ID_VF_BP2_620) || \
-	((pdev)->id.device_id == HINIC3_DEV_ID_VF_BP3_620) \
+	((pdev)->id.device_id == HINIC3_DEV_ID_VF_BP3_620) || \
+	((pdev)->id.device_id == HINIC3_DEV_ID_VF_SP560) || \
+	((pdev)->id.device_id == HINIC3_DEV_ID_HYPER_VF_SP560) \
 )
 
 u32 hinic3_hwif_read_reg(struct hinic3_hwif *hwif, u32 reg)
