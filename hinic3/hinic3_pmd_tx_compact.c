c1db1360913a78b00d65df86f9c572c09ff8d432
Jiacheng Ye <1409656437@qq.com>
2026-07-21 20:33:55 +0800

    [fix] 报文防攻击，当tso场景下，分片mbuf的长度小于声明的报文头长度时丢包
    [描述信息] 报文防攻击，当tso场景下，分片mbuf的长度小于声明的报文头长度时丢包
    [修改说明] 异常场景，需要丢弃报文
    
    Signed-off-by: Jiacheng Ye <1409656437@qq.com>

--- hinic3/hinic3_pmd_tx_compact.c
+++ hinic3/hinic3_pmd_tx_compact.c
@@ -882,6 +882,12 @@ static bool hinic3_is_tso_sge_valid(struct rte_mbuf *mbuf,
 	/* calculate the number of message payload frag, if it exceeds the hardware limit of 10 bits,
 	 * perform packet discard processing.
 	 */
+	if (unlikely(mbuf_head->data_len < wqe_info->payload_offset &&
+		     mbuf->nb_segs > HINIC3_NONTSO_PKT_MAX_SGE)) {
+		PMD_DRV_LOG(WARNING, "illegal pkt, payload offset (%u) > data len (%u).\n", 
+ 	 		    wqe_info->payload_offset, mbuf->data_len);
+		return false;
+	}
 	payload_len = mbuf_head->pkt_len - wqe_info->payload_offset;
 	if (unlikely(mbuf_head->tso_segsz == 0)) {
 		PMD_DRV_LOG(ERR, "TSO segment size is zero");
